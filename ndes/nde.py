a8cb015e7a2eda066a25342390e4c73f75517292
Tom Charnock <tom.charnock@iap.fr>
2018-09-05 13:46:11 +0200

    TensorFlow loss function (semi-working)

--- ndes/nde.py
+++ ndes/nde.py
@@ -1,12 +1,11 @@
-import theano
-import theano.tensor as T
+import tensorflow as tf
+import numpy as np
+import keras
 from keras import backend as K
 from keras.models import Sequential
 from keras.layers.core import Layer, Dense, Lambda
 import getdist
 from getdist import plots, MCSamples
-from ndes.losses import *
-import keras
 import emcee
 import matplotlib.pyplot as plt
 
@@ -31,40 +30,46 @@ class DelfiMixtureDensityNetwork():
         # Number of hiden units and activations
         self.n_hidden = n_hidden
         self.activations = activations
-        
+
         # Total number of outputs for the neural network
-        self.N = (self.D + self.D**2 + 1)*self.M
-    
+        self.N = int((self.D + self.D * (self.D + 1) / 2 + 1)*self.M)
+
+        # Constant used in loss function
+        #self.sq2piD = np.sqrt(2 * np.pi)**self.D
+        self.Dlog2pio2 = self.D*np.log(2. * np.pi) / 2.
+
         # Initialize the sequential Keras model
         self.mdn = Sequential()
-        
+
         # Add the (dense) hidden layers
         for i in range(len(self.n_hidden)):
             self.mdn.add(Dense(self.n_hidden[i], activation=self.activations[i], input_shape=(self.D,)))
-        
+
         # Linear output layer
         self.mdn.add(Dense(self.N, activation='linear'))
-        
+        #self.μ, self.Σ, self.α = self.mapping(self.mdn.layers[-1].output)
+
         # Compile the Keras model
-        self.mdn.compile(loss=neg_log_normal_mixture_likelihood,
+        self.mdn.summary()
+        self.mdn.compile(loss=self.neg_log_normal_mixture_likelihood,
                          optimizer='adam')
-            
+
         # Prior and asymptotic posterior
         self.prior = prior
         self.asymptotic_posterior = asymptotic_posterior
-        
+
         # Training data
         self.ps = []
         self.xs = []
         self.x_train = []
         self.y_train = []
         self.n_sims = 0
-        
+
         # MCMC chain parameters
         self.nwalkers = nwalkers
         self.posterior_chain_length = posterior_chain_length
         self.proposal_chain_length = proposal_chain_length
-            
+
         # MCMC samples of learned posterior
         self.posterior_samples = np.array([self.asymptotic_posterior.draw() for i in range(self.nwalkers*self.posterior_chain_length)])
         self.proposal_samples = np.array([self.asymptotic_posterior.draw() for i in range(self.nwalkers*self.proposal_chain_length)])
@@ -72,21 +77,21 @@ class DelfiMixtureDensityNetwork():
         # Simulator
         self.simulator = simulator
         self.simulator_args = simulator_args
-        
+
         # Fisher matrix and fiducial parameters
         self.Finv = Finv
         self.fisher_errors = np.sqrt(np.diag(self.Finv))
         self.theta_fiducial = theta_fiducial
-            
+
         # Data
         self.data = data
-    
+
         # Parameter names and ranges for plotting with GetDist
         self.names = names
         self.labels = labels
         self.ranges = ranges
         self.show_plot = show_plot
-    
+
         # Training loss, validation loss
         self.loss = []
         self.val_loss = []
@@ -110,7 +115,7 @@ class DelfiMixtureDensityNetwork():
         for i in range(self.n_procs):
             n_j_remain = n_jobs - n_j_allocated
             n_p_remain = self.n_procs - i
-            n_j_to_allocate = n_j_remain / n_p_remain
+            n_j_to_allocate = n_j_remain // n_p_remain
             if self.rank == i:
                 return range(n_j_allocated, \
                              n_j_allocated + n_j_to_allocate)
@@ -128,36 +133,36 @@ class DelfiMixtureDensityNetwork():
         else:
             target = target_distrib
         return target
-    
+
     # Log posterior
     def log_posterior(self, x):
-        
+
         if self.prior.pdf(x) == 0:
             return -1e300
         else:
             return self.log_likelihood((x - self.theta_fiducial)/self.fisher_errors) + np.log(self.prior.pdf(x))
-    
+
     # Log posterior
     def log_geometric_mean_proposal(self, x):
-        
+
         if self.prior.pdf(x) == 0:
             return -1e300
         else:
             return 0.5 * (self.log_likelihood((x - self.theta_fiducial)/self.fisher_errors) + 2 * np.log(self.prior.pdf(x)) )
-    
+
     # Run n_batch simulations
     def run_simulation_batch(self, n_batch, ps):
-        
+
         # Dimension outputs
         data_samples = np.zeros((n_batch, self.npar))
         parameter_samples = np.zeros((n_batch, self.npar))
-        
-        # Run samples assigned to each process, catching exceptions 
+
+        # Run samples assigned to each process, catching exceptions
         # (when simulator returns np.nan).
         i_prop = self.inds_prop[0]
         i_acpt = self.inds_acpt[0]
         err_msg = 'Simulator returns {:s} for parameter values: {} (rank {:d})'
-        while i_acpt <= self.inds_acpt[-1]:
+        while i_acpt <= self.inds_acpt[-1] and i_prop <= self.inds_prop[-1]:
             try:
                 sim = self.simulator(ps[i_prop,:], self.simulator_args)
                 if np.all(np.isfinite(sim.flatten())):
@@ -177,46 +182,48 @@ class DelfiMixtureDensityNetwork():
 
     # EMCEE sampler
     def emcee_sample(self, log_likelihood, x0, burn_in_chain=100, main_chain=100):
-    
+
         # Set up the sampler
         sampler = emcee.EnsembleSampler(self.nwalkers, self.D, log_likelihood)
-    
+
         # Burn-in chain
         pos, prob, state = sampler.run_mcmc(x0, burn_in_chain)
         sampler.reset()
-    
+
         # Main chain
         sampler.run_mcmc(pos, main_chain)
-    
+
         return sampler.flatchain
 
     # MDN log likelihood
     def log_likelihood(self, theta):
-    
+        # Predict means, covariances and weights given data
         y_out = self.mdn.predict(theta.astype(np.float32).reshape((1, self.D)))
-    
-        means = y_out[0, : self.D*self.M].reshape((self.M, self.D))
-        sigmas = y_out[0, self.D*self.M: self.D*self.M + self.M*self.D*self.D].reshape((self.M, self.D, self.D))
-        weights = np.exp(y_out[0, self.D*self.M + self.M*self.D*self.D:])/sum(np.exp(y_out[0, self.D*self.M + self.M*self.D*self.D:]))
-        like = 0
-        for i in range(self.M):
-            L = np.tril(sigmas[i,:,:], k=-1) + np.diag(np.exp(np.diagonal(sigmas[i,:,:])))
-            like += weights[i]*np.exp(-0.5*np.sum(np.dot(L.T, ((self.data - self.theta_fiducial)/self.fisher_errors - means[i,:]))**2))*np.prod(np.diag(L))/np.sqrt((2*np.pi)**self.D)
-    
-        if np.isnan(np.log(like)) == True:
+
+        # Extract means, covariances and weights
+        μ = y_out[0, :self.D*self.M].reshape((self.M, self.D))
+        Σ, det = self.covariance_matrix(y_out[0, self.D*self.M:self.M*(self.D*(self.D + 1) // 2 + self.D)].reshape((1, self.M, self.D*(self.D + 1) // 2)), use_tf = False)
+        α = np.exp(y_out[0, self.M*(self.D*(self.D + 1) // 2 + self.D):]) / np.sum(np.exp((y_out[0, self.M*(self.D*(self.D + 1) // 2 + self.D):])))
+
+        # Calculate likelihood
+        diff = (self.data - self.theta_fiducial)/self.fisher_errors-μ
+        like = np.sum(-0.5 *np.einsum("ijk,ik->i", Σ, diff**2.) + np.log(α) + np.log(det) - self.Dlog2pio2)
+        #like = np.sum(α * np.exp(-0.5*np.einsum("ijk,ik->i", Σ, diff**2.)) * det / np.sqrt((2 * np.pi) ** self.D))
+
+        if np.isnan(like) == True:
             return -1e300
         else:
-            return np.log(like)
+            return like
 
     def sequential_training(self, n_initial, n_batch, n_populations, proposal, \
                             safety = 5, plot = True, batch_size=100, \
                             validation_split=0.1, epochs=100, patience=20):
 
-        # Generate initial theta values from some broad proposal on 
+        # Generate initial theta values from some broad proposal on
         # master process and share with other processes. Overpropose
-        # by a factor of safety to (hopefully) cope gracefully with 
-        # the possibility of some bad proposals. Assign indices into 
-        # proposal array (self.inds_prop) and accepted arrays 
+        # by a factor of safety to (hopefully) cope gracefully with
+        # the possibility of some bad proposals. Assign indices into
+        # proposal array (self.inds_prop) and accepted arrays
         # (self.inds_acpt) to allow for easy MPI communication.
         if self.rank == 0:
             ps = np.array([proposal.draw() for i in range(safety * n_initial)])
@@ -254,14 +261,14 @@ class DelfiMixtureDensityNetwork():
                                    epochs=epochs, verbose=1, \
                                    validation_split=validation_split, \
                                    callbacks=[kcb])
-                        
+
             # Update the loss and validation loss
             self.loss = history.history['loss']
             self.val_loss = history.history['val_loss']
             self.loss_trace.append(history.history['loss'][-1])
             self.val_loss_trace.append(history.history['val_loss'][-1])
             self.n_sim_trace.append(self.n_sims)
-            
+
             # Generate posterior samples
             print('Sampling approximate posterior...')
             self.posterior_samples = \
@@ -278,15 +285,15 @@ class DelfiMixtureDensityNetwork():
 
         # Loop through a number of populations
         for i in range(n_populations):
-            
-            # Propose theta values on master process and share with 
-            # other processes. Again, ensure we propose more sets of 
+
+            # Propose theta values on master process and share with
+            # other processes. Again, ensure we propose more sets of
             # parameters than needed to cope with bad params.
             if self.rank == 0:
 
                 # Current population
                 print('Population {}/{}'.format(i+1, n_populations))
-        
+
                 # Sample the current posterior approximation
                 print('Sampling proposal density...')
                 self.proposal_samples = \
@@ -312,7 +319,7 @@ class DelfiMixtureDensityNetwork():
 
             # Train on master only
             if self.rank == 0:
-        
+
                 # Augment the training data
                 ps_batch = (ps_batch - self.theta_fiducial)/self.fisher_errors
                 xs_batch = (xs_batch - self.theta_fiducial)/self.fisher_errors
@@ -321,14 +328,14 @@ class DelfiMixtureDensityNetwork():
                 self.n_sims += n_batch
                 self.x_train = self.ps.astype(np.float32)
                 self.y_train = self.xs.astype(np.float32)
-        
+
                 # Train the network on these initial simulations
                 history = self.mdn.fit(self.x_train, self.y_train,
                                        batch_size=batch_size, \
                                        epochs=epochs, verbose=1, \
                                        validation_split=validation_split, \
                                        callbacks=[kcb])
-                       
+
                 # Update the loss and validation loss
                 self.loss = np.concatenate([self.loss, history.history['loss']])
                 self.val_loss = np.concatenate([self.val_loss, history.history['val_loss']])
@@ -373,7 +380,7 @@ class DelfiMixtureDensityNetwork():
                 self.triangle_plot([self.posterior_samples], \
                                     savefig=True, \
                                     filename='seq_train_post_final.pdf')
-    
+
     def fisher_pretraining(self, n_batch, proposal, plot=True, batch_size=100, validation_split=0.1, epochs=100, patience=20):
 
         # Train on master only
@@ -406,7 +413,7 @@ class DelfiMixtureDensityNetwork():
             history = self.mdn.fit(self.x_train, self.y_train,
               batch_size=batch_size, epochs=epochs, verbose=1, validation_split=validation_split, callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=patience, verbose=0, mode='auto')])
             print("Done.")
-            
+
             # Initialization for the EMCEE sampling
             x0 = [self.posterior_samples[-i,:] for i in range(self.nwalkers)]
 
@@ -444,7 +451,7 @@ class DelfiMixtureDensityNetwork():
                 ax.set_xticklabels(xtl, rotation=45)
         plt.tight_layout()
         plt.subplots_adjust(hspace=0, wspace=0)
-        
+
         if savefig:
             print('Saving ' + filename)
             plt.savefig(filename)
@@ -453,3 +460,51 @@ class DelfiMixtureDensityNetwork():
         else:
             plt.close()
 
+    # Build lower triangular from network output (also calculate determinant)
+    def covariance_matrix(self, σ, use_tf = True):
+        Σ = []
+        det = 1
+        start = 0
+        end = 1
+        for i in range(self.D):
+            if use_tf:
+                exp_val = tf.exp(σ[:, :, end-1])
+            else:
+                exp_val = np.exp(σ[:, :, end-1])
+            det *= exp_val
+            if i > 0:
+                if use_tf:
+                    Σ.append(tf.pad(tf.concat([σ[:, :, start:end-1], tf.expand_dims(exp_val, -1)], -1), [[0, 0], [0, 0], [0, self.D-i-1]]))
+                else:
+                    Σ.append(np.pad(np.concatenate([σ[:, :, start:end-1], exp_val[:, :, np.newaxis]], -1), [[0, 0], [0, 0], [0, self.D-i-1]], "constant"))
+            else:
+                if use_tf:
+                    Σ.append(tf.pad(tf.expand_dims(exp_val, -1), [[0, 0], [0, 0], [0, self.D-i-1]]))
+                else:
+                    Σ.append(np.pad(exp_val[:, :, np.newaxis], [[0, 0], [0, 0], [0, self.D-i-1]], "constant"))
+            start = end
+            end += i + 2
+        if use_tf:
+            Σ = tf.transpose(tf.stack(Σ), (1, 2, 0, 3))
+        else:
+            Σ = np.swapaxes(np.swapaxes(Σ, 0, 1), 1, 2)[0]
+            det = det[0]
+        return Σ, det
+
+    # Split network output into means, covariances and weights (also returns determinant of covariance)
+    def mapping(self, parameters):
+        μ, Σ, α = tf.split(parameters, [self.M * self.D, self.M * self.D * (self.D + 1) // 2, self.M], 1)
+        μ = tf.reshape(μ, (-1, self.M, self.D))
+        Σ, det = self.covariance_matrix(tf.reshape(Σ, (-1, self.M, self.D * (self.D + 1) // 2)))
+        α = tf.nn.softmax(α)
+        return μ, Σ, α, det
+
+    # Loss function
+    def neg_log_normal_mixture_likelihood(self, true, parameters):
+        μ, Σ, α, det = self.mapping(parameters)
+        diff = tf.expand_dims(true, 1) - μ
+        return tf.reduce_mean(tf.reduce_sum(0.5 * tf.einsum("ijkl,ijk->ij", Σ, tf.square(diff)) - tf.log(α) - tf.log(det) + self.Dlog2pio2, 1))
+
+    def log_sum_exp(self, x, axis=None):
+        x_max = tf.reduce_max(x, axis=axis, keepdims=True)
+        return tf.log(tf.reduce_sum(tf.exp(x - x_max), axis=axis, keepdims=True)) + x_max
